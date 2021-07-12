57d515316499fe9cc46c3fa81c5c2a8621bc01d7
Flor Condori <condorihuamanif@gmail.com>
2021-07-11 22:46:08 -0500

    iniciando con VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <px-header />
+    <router-view class="container px-5"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from "@/components/PxHeader";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    PxHeader,
   },
 };
 </script>
